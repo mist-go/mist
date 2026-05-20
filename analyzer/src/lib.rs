ff38c809e736b8ec5217f3fe814b343d501d4f42
Selimaj Dev <klesti.selimay@icloud.com>
2026-05-20 10:17:14 +0200

    Simple completion example

--- analyzer/src/lib.rs
+++ analyzer/src/lib.rs
@@ -10,13 +10,35 @@ struct Backend {
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+        let mut res = InitializeResult::default();
+
+        res.capabilities.completion_provider = Some(CompletionOptions {
+            resolve_provider: None,
+            trigger_characters: None,
+            all_commit_characters: None,
+            work_done_progress_options: WorkDoneProgressOptions {
+                work_done_progress: None,
+            },
+            completion_item: None,
+        });
+
+        Ok(res)
     }
 
     async fn initialized(&self, _: InitializedParams) {
         self.client
             .log_message(MessageType::INFO, "server initialized!")
             .await;
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        self.client
+            .log_message(MessageType::INFO, "getting completion")
+            .await;
+
+        Ok(Some(CompletionResponse::Array(vec![
+            CompletionItem::new_simple("new".to_string(), "The new keyword".to_string()),
+        ])))
     }
 
     async fn shutdown(&self) -> Result<()> {
